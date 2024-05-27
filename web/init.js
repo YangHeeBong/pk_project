743141d0ad31eab9c9d9d5c511e2961a15a3e661
YangHeeBong <82924515+YangHeeBong@users.noreply.github.com>
2024-05-27 10:59:43 +0900

    기본 지도 업데이트

---
+++ web/init.js
@@ -0,0 +1,22 @@
+﻿/**
+ * 엔진 로드 후 실행할 초기화 함수(Module.postRun)
+ */
+var Module = {
+    locateFile: function (path, prefix) {
+        return "./" + prefix + path;
+    },
+    postRun: [start],
+};
+
+(function () {
+    var file = "./XDWorldEM.js";
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/wasm");
+    xhr.open("GET", file, true);
+    xhr.onload = function () {
+        var script = document.createElement("script");
+        script.innerHTML = xhr.responseText;
+        document.body.appendChild(script);
+    };
+    xhr.send(null);
+})();
